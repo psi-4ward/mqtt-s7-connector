4c52b26bf4476ccd67fe9251d61a39005769371f
Martijn <martijn.diks@gmail.com>
2024-06-12 15:51:16 +0200

    loglevel can now be set from the commandline to be able to set it in the addon configuration

--- config_handler.js
+++ config_handler.js
@@ -1,6 +1,7 @@
 let commander;
 let fs;
 let yaml;
+
 commander = require('commander');
 fs = require('fs');
 yaml = require('yaml');
@@ -12,6 +13,7 @@ let config = function() {
         .name('mqtt-s7-connector')
         .option('-c, --config <value>', 'Overwrite the default config file location. e.g. /etc/mqtt-s7-connector/config.json')
         .option('-y, --yaml', 'Takes yaml file as config file')
+        .option('-l, --loglevel <value>', 'Sets the log level, Default=4 >>> 0: Trace, 1: Debug, 2: Info, 3: Notice, 4: Warning, 5: Error, 6: Fatal')
         .parse(process.argv);
 
 // if --config argument is not specified use the config file as originally build
@@ -28,6 +30,13 @@ let config = function() {
             return require('./config');
         }
     }
+
+    if (commander.opts().loglevel !== undefined) {
+        global.log_level = commander.opts().loglevel;
+    } else {
+        global.log_level = 4;
+    }
+
 }
 
 module.exports = {
