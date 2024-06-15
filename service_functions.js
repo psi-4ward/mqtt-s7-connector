13fb3cf4d6d06a733ad95a2f808985d990c480fd
Martijn <martijn.diks@gmail.com>
2024-06-15 13:00:14 +0200

    new try for colors

--- service_functions.js
+++ service_functions.js
@@ -21,54 +21,54 @@ function datetime() {
 	return year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + second;
 }
 
-const red = '\\x1b[31m';
-const green = '\\x1b[32m';
-const yellow = '\\x1b[33m';
-const blue = '\\x1b[34m';
-const cyan = '\\x1b[36m';
-const redBright = '\\x1b[39m';
-const blueBright = '\\x1b[42m';
-const reset = '\\x1b[0m';
+const red = '\\x1b[31m ';
+const green = '\\x1b[32m ';
+const yellow = '\\x1b[33m ';
+const blue = '\\x1b[34m ';
+const cyan = '\\x1b[36m ';
+const redBright = '\\x1b[39m ';
+const blueBright = '\\x1b[42m ';
+const reset = ' \\x1b[0m';
 
 module.exports.trace = function trace(msg) {
 	if (global.log_level < 1) {
-		console.log(green + datetime() + "## TRACE   ## " + msg + reset);
+		console.log(green, datetime() + " ## TRACE   ## " + msg, reset);
 	}
 }
 
 module.exports.debug = function debug(msg) {
 	if (global.log_level <= 1) {
-		console.log(cyan + datetime() + "## DEBUG   ## " + msg + reset);
+		console.log(cyan, datetime() + " ## DEBUG   ## " + msg, reset);
 	}
 }
 
 module.exports.info = function info(msg) {
 	if (global.log_level <= 2) {
-		console.log(blue + datetime() + "## INFO    ## " + msg + reset);
+		console.log(blue, datetime() + " ## INFO    ## " + msg, reset);
 	}
 }
 
 module.exports.notice = function notice(msg) {
 	if (global.log_level <= 3) {
-		console.log(blueBright + datetime() + "## NOTICE  ## " + msg + reset);
+		console.log(blueBright, datetime() + " ## NOTICE  ## " + msg, reset);
 	}
 }
 
 module.exports.warning = function warning(msg) {
 	if (global.log_level <= 4) {
-		console.log(yellow + datetime() + "## WARNING ## " + msg + reset);
+		console.log(yellow, datetime() + " ## WARNING ## " + msg, reset);
 	}
 }
 
 module.exports.error = function error(msg) {
 	if (global.log_level <= 5) {
-		console.log(red + datetime() + "## ERROR  ## " + msg + reset)
+		console.log(red, datetime() + " ## ERROR  ## " + msg, reset)
 		throw new Error(msg);
 	}
 	process.exit(-1);
 }
 
 module.exports.fatal = function fatal(msg) {
-	console.log(redBright + datetime() + "## FATAL  ## " + msg + reset)
+	console.log(redBright, datetime() + " ## FATAL  ## " + msg, reset)
 	throw new Error(msg);
 }
