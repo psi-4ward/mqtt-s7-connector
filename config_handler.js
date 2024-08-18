7b997a1d3287caa2904c92f77d10ad2d8865a4fb
Martijn <martijn.diks@gmail.com>
2024-08-18 14:08:02 +0200

    added version --addonversion option

--- config_handler.js
+++ config_handler.js
@@ -1,14 +1,18 @@
-let sf = require('./service_functions.js');
+// noinspection JSUnresolvedReference
 
-let commander = require('commander');
+let commander = require("commander");
+let sf = require('./service_functions.js');
 let fs = require('fs');
 let yaml = require('yaml');
 
+
 let config = function() {
     // add --config option to specify a config file path
+    // add --version option to set the version for the "origin" section of the discovery topic
     // add --loglevel option to specify the log level
     commander.name('mqtt-s7-connector')
         .option('-c, --config <value>', 'Overwrite the default config file location. e.g. /etc/mqtt-s7-connector/config.json')
+        .option('-v, --addonversion <value>', 'Set the version for the "origin" section of the discovery topic')
         .option('-l, --loglevel <value>', 'Sets the log level, Default=4 >>> 0: Trace, 1: Debug, 2: Info, 3: Notice, 4: Warning, 5: Error, 6: Fatal')
         .parse(process.argv);
 
@@ -17,10 +21,15 @@ let config = function() {
     } else {
         global.log_level = 4;
     }
-
     console.log('## INIT   ## log level is set to: ' + global.log_level);
 
-// if --config argument is not specified use the config file as originally build
+    if (commander.opts().addonversion !== undefined) {
+        global.addon_version = commander.opts().addonversion;
+    } else {
+        global.addon_version = "unset";
+    }
+
+    // if --config argument is not specified use the config file as originally build
     if (commander.opts().config !== undefined) {
         let extension = commander.opts().config.slice(-4);
 
